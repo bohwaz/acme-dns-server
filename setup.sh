1d213283dc34721667d9f84fb1d8c4fd9e4f6c51
Christian Kraus <hanzi@hanzi.cc>
2019-03-08 13:36:39 +0800

    Fix calling setup.sh from different pwd

--- setup.sh
+++ setup.sh
@@ -3,14 +3,14 @@
 DIR=`dirname "$0"`
 REALDIR=`realpath $DIR`
 
-sudo cp acme-dns-server.py /usr/bin/acme-dns-server.py
+sudo cp $REALDIR/acme-dns-server.py /usr/bin/acme-dns-server.py
 sudo chown root:root /usr/bin/acme-dns-server.py
 sudo chmod 0755 /usr/bin/acme-dns-server.py
 
 sudo mkdir -p /opt/acme-challenge/records
 
-sudo cp acme-dns-server.config /etc/default/acme-dns-server
-sudo cp acme-dns-server.service /etc/systemd/system/acme-dns-server.service
+sudo cp $REALDIR/acme-dns-server.config /etc/default/acme-dns-server
+sudo cp $REALDIR/acme-dns-server.service /etc/systemd/system/acme-dns-server.service
 sudo chown root:root /etc/systemd/system/acme-dns-server.service /etc/default/acme-dns-server
 sudo chmod 0644 /etc/systemd/system/acme-dns-server.service /etc/default/acme-dns-server
 
@@ -19,5 +19,5 @@ sudo systemctl enable acme-dns-server.service
 sudo systemctl start acme-dns-server.service
 
 if [ -d "~/.acme.sh/" ]; then
-    cp dns_pythondnsd.sh ~/.acme.sh/
+    cp $REALDIR/dns_pythondnsd.sh ~/.acme.sh/
 fi
